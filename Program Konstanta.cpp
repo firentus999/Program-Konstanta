3cf938ba60e07b1c3525a2f2822e3190cb6aaf7a
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-12 09:29:59 +0700

    Program

---
+++ Program Konstanta.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+const float phi=3.14;
+const char nama []="firentus agustone";
+int main()
+{
+    printf("%f\n",phi);
+float luas=7*7*phi;
+    printf("%f\n",luas);
+    printf("%s\n",nama);
+
+    getch();
+}
